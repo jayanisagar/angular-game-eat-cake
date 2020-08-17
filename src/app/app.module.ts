96d1865d6985e84c2fd23bdc28990721034fb097
sagar.jayani <sagar.jayani@bizotics.com>
2020-08-18 01:39:43 +0530

    project added

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { FormsModule } from '@angular/forms';
+import { ToMatrixPipe } from './to-matrix.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameComponent,
+    ToMatrixPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  inputMatrixNumbar: number;
+  metrixObj: any[] = [];
+  stepCount: number = 0;
+  currentPlayerPosition: any = { x: 0, y: 0 };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    
+    this.metrixObj = [];
+    this.stepCount = 0;
+    for (let index = 0; index < this.inputMatrixNumbar; index++) {
+      let col = []
+      for (let j = 0; j < this.inputMatrixNumbar; j++) {
+        col.push('');
+      }
+      this.metrixObj.push(col);
+    }
+
+    this.currentPlayerPosition.x = (Math.round(this.inputMatrixNumbar / 2) - 1);
+    this.currentPlayerPosition.y = (Math.round(this.inputMatrixNumbar / 2) - 1);
+    this.metrixObj[this.currentPlayerPosition.x][this.currentPlayerPosition.y] = "p";
+
+    let arr: any[] = this.getRandomNumber();
+    let arr1: any[] = this.getRandomNumber();
+    for (let i = 0; i < arr.length; i++) {
+      if (this.currentPlayerPosition.x == arr[i] && this.currentPlayerPosition.y == arr1[i]) {
+        this.metrixObj[arr[i] + 1][arr1[i]] = "e";
+      } else {
+        this.metrixObj[arr[i]][arr1[i]] = "e";
+      }
+    }
+  }
+
+  getRandomNumber() {
+    var arr = [];
+    while (arr.length < this.inputMatrixNumbar) {
+      var r = Math.floor(Math.random() * this.inputMatrixNumbar);
+      if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+  }
+
+  @HostListener('keydown', ['$event']) onKeyUp(e) {
+
+    if(this.metrixObj.length) {
+      this.metrixObj[this.currentPlayerPosition.x][this.currentPlayerPosition.y] = "-";
+    }
+
+    switch (e.keyCode) {
+      case 38: // up
+        if(this.currentPlayerPosition.x > 0)
+          this.currentPlayerPosition.x--;
+        this.metrixObj[this.currentPlayerPosition.x][this.currentPlayerPosition.y] = "p";
+        this.stepCount++;
+        break;
+      case 37: // left
+        if(this.currentPlayerPosition.y > 0)
+          this.currentPlayerPosition.y--;
+        this.metrixObj[this.currentPlayerPosition.x][this.currentPlayerPosition.y] = "p";
+        this.stepCount++;
+        break;
+      case 40: // down
+        if(this.currentPlayerPosition.x < (this.inputMatrixNumbar - 1))
+          this.currentPlayerPosition.x++;
+        this.metrixObj[this.currentPlayerPosition.x][this.currentPlayerPosition.y] = "p";
+        this.stepCount++;
+        break;
+      case 39: // right
+        if(this.currentPlayerPosition.y < (this.inputMatrixNumbar - 1))
+          this.currentPlayerPosition.y++;
+        this.metrixObj[this.currentPlayerPosition.x][this.currentPlayerPosition.y] = "p";
+        this.stepCount++;
+        break;
+    }
+  }
+
+}
